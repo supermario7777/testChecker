0ee75212ef6c0124e617ad0cf46c5d2fb7eb1586
Admin <syubebayevm@gmail.com>
2023-06-25 18:10:14 +0600

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,7 @@
 import './App.css';
 import React from 'react';
 import './components/CameraModal/CameraModal'
-import CameraModal1 from './components/CameraModal1';
-// import CameraModal from './components/CameraModal/CameraModal';
-// import CameraComponent from './components/CameraComponent/CameraComponent';
-// import CameraModal1 from './components/CameraModal/CameraModal';
+import CameraModal from './components/CameraModal/CameraModal';
 
 
 function App() {
@@ -12,8 +9,7 @@ function App() {
   return (
     <div className="App">
       <div>
-        {/* <CameraModal/> */}
-        <CameraModal1/>
+        <CameraModal/>
       </div>
     </div>
   );
